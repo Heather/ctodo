254305c4954aabc4060eafa438b20eeabd0a31eb
Sholy <Sholy@NenGraphy.com>
2012-05-16 08:28:23 +0400

    encryption column

--- sholy.c
+++ sholy.c
@@ -160,7 +160,7 @@ int main(int argc, char *argv[]) {
 		return -1;
 	    }
 	  if (  strcmp(argv[1],"initdb") == 0 ) {
-		char create_table[100] = "CREATE TABLE IF NOT EXISTS TODO (id INTEGER PRIMARY KEY,text TEXT NOT NULL)";
+		char create_table[100] = "CREATE TABLE IF NOT EXISTS TODO (id INTEGER PRIMARY KEY,text TEXT NOT NULL,enc INTEGER NOT NULL)";
 		retval = sqlite3_exec(handle,create_table,0,0,0);
 	    }
 	  else if ( strcmp(argv[1],"write") == 0 ) {
@@ -171,7 +171,7 @@ int main(int argc, char *argv[]) {
 		  queries[ind++] = "SELECT MAX(id) FROM TODO GROUP BY (text)";
 		  retval = sqlite3_prepare_v2(handle,queries[ind-1],-1,&stmt,0);
 		  if(retval) {
-			printf("Inserting data to DB Failed\n");
+			printf("Inserting data to DB Failed, run initdb first\n");
 			return -1;
 	        }
 		  int last = 0;
@@ -184,10 +184,10 @@ int main(int argc, char *argv[]) {
           char enc[16];
 		  if (encryption!=0) {
 			memcpy( enc, Encrypt(key,argv[2]), 16 );
-			sprintf(queries[ind++], "INSERT INTO TODO VALUES(%d,'%s')", last, enc);
+			sprintf(queries[ind++], "INSERT INTO TODO VALUES(%d,'%s',1)", last, enc);
 		    }
 		  else {
-			sprintf(queries[ind++], "INSERT INTO TODO VALUES(%d,'%s')", last, argv[2]);
+			sprintf(queries[ind++], "INSERT INTO TODO VALUES(%d,'%s',0)", last, argv[2]);
 		    }
 		  retval = sqlite3_exec(handle,queries[ind-1],0,0,0);
 		  }
@@ -210,24 +210,26 @@ int main(int argc, char *argv[]) {
 		  sprintf(queries[ind++], "SELECT id, text FROM TODO WHERE id = %s", argv[2]);
 		  }
 		else {
-	      queries[ind++] = "SELECT id, text from TODO";
+	      queries[ind++] = "SELECT id, text, enc from TODO";
 		  }
 	    retval = sqlite3_prepare_v2(handle,queries[ind-1],-1,&stmt,0);
 	    if(retval) {
-		  printf("Selecting data from DB Failed\n");
+		  printf("Selecting data from DB Failed, run initdb first\n");
 		  return -1;
 	      }
 		printf("======================================================\n");
 		unsigned char key[32];
 		memset( key, 0, 32 );
         char dec[16];
+		int encryption = 0;
 		while (sqlite3_step(stmt) == SQLITE_ROW) {
-		  if (encryption!=0) {
+		  printf("%s | ", sqlite3_column_text(stmt, 0));
+		  encryption =  atoi(sqlite3_column_text(stmt, 2));
+		  if (encryption == 1) {
 		    memcpy( dec, Decrypt(key, (char *)sqlite3_column_text(stmt, 1) ), 16 );
 		    printf("%s\n", dec);
-		    }
+			}
 		  else {
-			printf("%s | ", sqlite3_column_text(stmt, 0));
 			printf("%s\n", sqlite3_column_text(stmt, 1));
 		    }
 	      }
