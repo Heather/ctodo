777dda3487739860a364186c3aa2359237a232c0
Ash <ASHhrly@gmail.com>
2012-11-02 10:49:48 +0400

    nn

--- todo.c
+++ todo.c
@@ -1,5 +1,5 @@
 /*          todo - Light TODO list
-          Copyright (C)  2012  nCdy
+          Copyright (C)  2012  Ash Harley
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public
