a10b91b8bcfc81e686e04c06318671da1442a95e
Cynede <cynede@outlook.com>
2012-11-21 09:40:26 +0400

    native -> CLR wrapper

---
+++ win/nwrap/nwrap.cpp
@@ -0,0 +1,44 @@
+// This is the main DLL file.
+
+#include "stdafx.h"
+
+#include "nwrap.h"
+
+namespace nwrap {
+    void  todo::n_initdb() {
+        todo_initdb();
+        }
+    void  todo::n_version() {
+        todo_version();
+        }
+    void  todo::n_help(char* argv) {
+        todo_help(argv);
+        }
+    int  todo::n_set(char** argv, int argc) {
+        return todo_set(argv, argc);
+        }
+    int  todo::n_sync(char** argv) {
+        return todo_sync(argv);
+        }
+    void  todo::n_edit(char** argv, int argc) {
+        todo_edit(argv, argc);
+        }
+    void  todo::n_swap(char** argv) {
+        todo_swap(argv);
+        }
+    void  todo::n_mv(char** argv) {
+        todo_mv(argv);
+        }
+    void  todo::n_clean() {
+        todo_clean();
+        }
+    void  todo::n_rm(char** argv) {
+        todo_rm(argv);
+        }
+    int  todo::n_read(char** argv, int argc) {
+        return todo_read(argv, argc);
+        }
+    int  todo::n_write(char** argv, int argc) {
+        return todo_write(argv, argc);
+        }
+    }
---
+++ win/nwrap/nwrap.h
@@ -0,0 +1,24 @@
+#pragma once
+
+extern "C" {
+    #include "ctodo.h"
+    }
+
+using namespace System;
+
+namespace nwrap {
+	public ref class todo {
+        void n_initdb();
+        void n_version();
+        void n_help(char* argv);
+        int n_set(char** argv, int argc);
+        int n_sync(char** argv);
+        void n_edit(char** argv, int argc);
+        void n_swap(char** argv);
+        void n_mv(char** argv);
+        void n_clean();
+        void n_rm(char** argv);
+        int n_read(char** argv, int argc);
+        int n_write(char** argv, int argc);
+	    };
+    }
