b669e285d38f33c7c3a1aef05467daaa533583ec
Heather Cynede <heather@cynede.net>
2013-03-19 08:58:24 +0400

    +

--- build.bat
+++ build.bat
@@ -1,2 +1,10 @@
 @echo off
+make -f Makefile.win clean
 make -f Makefile.win
+
+todo.exe --help
+echo ---------------------------------------------------
+todo.exe
+echo ---------------------------------------------------
+
+pause
